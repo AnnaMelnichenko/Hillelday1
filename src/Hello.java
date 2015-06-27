9efaa12391f09be210e012aead18a7ad3b9f0268
AnnetMelnichenko <annetpistruil@gmail.com>
2015-06-27 20:08:31 +0200

    initial commit

---
+++ src/Hello.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Annet on 18.06.2015.
+ */
+public class Hello {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, please input your text");
+        System.out.print("Java");
+    }
+}
